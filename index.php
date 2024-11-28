0bd8224cbce78b6b6ea013b9c636c8e96698ec84
blue🐰 <75032462+BlueBunnex@users.noreply.github.com>
2024-11-27 21:43:34 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,17 +4,17 @@
 	<title>localbooru</title>
 	<style>
 		body { font-family: sans-serif; color: #ddd; background: #212; text-align: center; }
-		a { color: #ea2; }
-		h1 { color: #ea2; font-size: 6em; }
+		a { color: #ea2; text-decoration: none; }
+		h1 { color: #ea2; font-size: 6em; margin-bottom: 0; }
 
 		img { border: 2px solid transparent; height: 200px; }
 	</style>
 </head>
 <body>
 
-	<h1>localbooru</h1>
+	<h1>bunsbooru</h1>
 
-	<div>
+	<nav>
 		<?php
 			
 			$dbString = file_get_contents('db.json');
@@ -23,22 +23,10 @@
 			// insert button to each board
 			foreach ($db["boards"] as $board) {
 
-				if (array_key_exists($board, $db) && count($db[$board]) > 0) {
-
-					// if board entry already exists in json, pog
-					$ids = array_keys($db[$board]);
-					$id = $ids[0];
-
-					echo "<a href='board/$board' style='display: inline-block; width: 200px; aspect-ratio: 1; background-image: url(\"image_db/$id.jpg\"); background-size: cover; background-position: center; color: black; font-size: 2em; text-shadow: 0 0 4px white;'>$board</a>";
-
-				} else {
-
-					// if board entry doesn't exist in json, or it's empty, the button can't have an image background
-					echo "<a href='board/$board' style='display: inline-block; width: 200px; aspect-ratio: 1; background: pink; color: black; font-size: 2em; text-shadow: 0 0 4px white;'>$board</a>";
-				}
+				echo "[ <a href='board/$board'>$board</a> ] ";
 			}
 		?>
-	</div>
+	</nav>
 
 </body>
 </html>
