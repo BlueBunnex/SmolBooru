b3b2dc75300712bc494735ad282fddcee294f35c
blue🐰 <75032462+BlueBunnex@users.noreply.github.com>
2024-11-27 22:58:46 -0500

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<style>
 		body { font-family: sans-serif; color: #ddd; background: #212; text-align: center; }
 		a { color: #ea2; text-decoration-style: dotted; }
-		h1 { color: #ea2; font-size: 6em; margin-bottom: 0; }
+		h1 { color: #ea2; font-size: 3em; margin: 1rem; }
 
 		img { border: 2px solid transparent; height: 200px; }
 	</style>
@@ -55,18 +55,19 @@
 
 				} else {
 
-					echo "[ <a href='/board/$board'>$board</a> ] ";
+					echo "[ <a href='/$board'>$board</a> ] ";
 				}
 			}
 		?>
 	</nav>
+
+	<br><br><br>
 
 	<?php
 
 		if ($page_type == "home") {
 
 			// just show a random image
-			echo "<br><br><br><br><br><br>";
 			echo "<img style='height: 300px;' src='image_db/" . array_keys($db["hourglass"])[0] . ".jpg'>";
 			echo "<br>A random image!";
 
@@ -74,7 +75,36 @@
 
 		else if ($page_type == "board") {
 
-			echo "board";
+			$response       = "red";
+			$response_color = "red";
+
+			echo `<fieldset style="width: 30em; margin: auto; text-align: left; border-color: #545;">
+				<legend>Add to booru</legend>
+				<form method="post">
+					<input type="text" name="file" placeholder="File URL... (I'll replace this with file upload later but it's hard sob)" style="width: 100%;">
+
+					<br><br>
+
+					<input type="submit" value="Post">
+					<span style="color: <?php echo $response_color; ?>;"><?php echo $response; ?></span>
+				</form>
+			</fieldset>
+
+			<br><br><br>`;
+
+			// add image to json
+			$dbString = file_get_contents('db.json');
+			$db = json_decode($dbString, true);
+
+			echo "<h2><span style='font-weight: 400;'>Images in</span> $board</h2>";
+
+			if ($db[$board] != null) {
+
+				foreach (array_keys($db[$board]) as $id) {
+
+					echo "<a href='/$board/$id'><img src='/image_db/$id.jpg'></a>";
+				}
+			}
 
 		}
 	
